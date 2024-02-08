c003aa60695b01c49bb823f6159260ed0e78eefc
Stanislav <9323354@gmail.com>
2024-02-08 20:41:11 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+const datainfo = `[
+    {
+        "id": "iphone15",
+        "url": "https://cybersport.metaratings.ru/storage/images/b3/54/b3548568bbac70bd7438956d90e4518d.jpg",
+        "width": 500,
+        "height": 334
+    },
+    {
+        "id": "iphone14",
+        "url": "https://www.ixbt.com/img/n1/news/2022/5/4/7115a2bafa6f4c9aa86578706df73013_large.png",
+        "width": 796,
+        "height": 1061 
+    },
+    {
+        "id": "iphone13",
+        "url": "https://i.pinimg.com/originals/ab/9d/ac/ab9dacfd545fead8d2118b7e984cb438.jpg",
+        "width": 765,
+        "height": 1024 
+    }
+]`;
